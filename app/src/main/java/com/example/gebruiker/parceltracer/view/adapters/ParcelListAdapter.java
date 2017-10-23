aaef4992cffe1d1836a22dfce53648186da1df41
Wietse Collaer <wietse.collaer@live.com>
2017-10-23 20:57:30 +0200

    Layout startscherm

---
+++ app/src/main/java/com/example/gebruiker/parceltracer/view/adapters/ParcelListAdapter.java
@@ -0,0 +1,62 @@
+package com.example.gebruiker.parceltracer.view.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.gebruiker.parceltracer.DummyClasses.ParcelDummy;
+import com.example.gebruiker.parceltracer.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ParcelListAdapter extends ArrayAdapter<ParcelDummy> {
+    static final String LOG_TAG = ParcelItemHolder.class.getSimpleName();
+    private LayoutInflater inflater;
+
+    public ParcelListAdapter(Context context, ArrayList<ParcelDummy> parcels){
+        super(context, 0, parcels);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ParcelDummy dummy = getItem(position);
+
+        ParcelItemHolder itemHolder;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.parcel_list_item, parent, false);
+            itemHolder = new ParcelItemHolder(convertView);
+            convertView.setTag(itemHolder);
+        }
+        else{
+            itemHolder = (ParcelItemHolder) convertView.getTag();
+        }
+        Log.d(LOG_TAG, "Hier geweest" + dummy.getName());
+        itemHolder.itemName.setText(dummy.getName());
+        itemHolder.itemStatus.setText(dummy.getStatus());
+
+        return convertView;
+    }
+
+    static class ParcelItemHolder {
+        @BindView(R.id.parcel_list_item_itemName)
+        TextView itemName;
+
+        @BindView(R.id.parcel_list_item_itemStatus)
+        TextView itemStatus;
+
+        public ParcelItemHolder(View view){
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/example/gebruiker/parceltracer/view/fragments/ParcelOverviewFragment.java
+++ app/src/main/java/com/example/gebruiker/parceltracer/view/fragments/ParcelOverviewFragment.java
@@ -8,9 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.example.gebruiker.parceltracer.DummyClasses.ParcelDummy;
 import com.example.gebruiker.parceltracer.R;
+import com.example.gebruiker.parceltracer.view.adapters.ParcelListAdapter;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ParcelOverviewFragment extends Fragment {
     @BindView(R.id.parcel_list)
@@ -28,7 +33,15 @@ public class ParcelOverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.overview_fragment, container, false);
-//        ButterKnife.bind(this, view);
+        ButterKnife.bind(this, view);
+
+        //TODO replace with real data from the api.
+        ArrayList<ParcelDummy> dummies = new ArrayList<>();
+        dummies.add(new ParcelDummy("Samsung s8", "received"));
+        dummies.add(new ParcelDummy("Iphone 8", "sent"));
+
+        ParcelListAdapter adapter = new ParcelListAdapter(getContext(), dummies);
+        parcelList.setAdapter(adapter);
 
         return view;
     }
